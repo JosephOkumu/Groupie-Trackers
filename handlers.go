d31a9ca9fe148b9795ca12290dbb6fc193da8fb0
JosephOkumu <jokumu25@gmail.com>
2024-08-14 22:31:17 +0300

    Adding handler funcs for index and artist endpoints

---
+++ handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "net/http"
+    "log"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    var artists []Artist
+    err := fetchAPI("https://api.example.com/artists", &artists)
+    if err != nil {
+        http.Error(w, "Failed to load artists", http.StatusInternalServerError)
+        return
+    }
+    err = tmpl.ExecuteTemplate(w, "index.html", artists)
+    if err != nil {
+        log.Println(err)
+    }
+}
+
+func artistHandler(w http.ResponseWriter, r *http.Request) {
+    id := r.URL.Query().Get("id")
+    if id == "" {
+        http.Error(w, "Artist ID is required", http.StatusBadRequest)
+        return
+    }
+
+    var artist Artist
+    err := fetchAPI("https://api.example.com/artists/"+id, &artist)
+    if err != nil {
+        http.Error(w, "Failed to load artist", http.StatusInternalServerError)
+        return
+    }
+
+    err = tmpl.ExecuteTemplate(w, "artist.html", artist)
+    if err != nil {
+        log.Println(err)
+    }
+}
