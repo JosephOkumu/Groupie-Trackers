64fa5611d931265a3b1bd39719275a767c4b46dd
josotieno <jokumu25@gmail.com>
2024-08-27 10:37:26 +0300

    Chore: Eliminating redundancy

--- services.go
+++ services.go
@@ -16,37 +16,3 @@ func fetchAPI(url string, target interface{}) error {
 
 }
 
-// Fetch and organize data
-func FetchAndOrganizeData() ([]ArtistInfo, error) {
-	var artists []Artist
-	var locations []Location
-	var dates []Date
-	var relations []Relation
-
-	// Fetch data from APIs
-	if err := fetchAPI("https://groupietrackers.herokuapp.com/api/artists", &artists); err != nil {
-		return nil, err
-	}
-	if err := fetchAPI("https://groupietrackers.herokuapp.com/api/locations", &locations); err != nil {
-		return nil, err
-	}
-	if err := fetchAPI("https://groupietrackers.herokuapp.com/api/dates", &dates); err != nil {
-		return nil, err
-	}
-	if err := fetchAPI("https://groupietrackers.herokuapp.com/api/relation", &relations); err != nil {
-		return nil, err
-	}
-
-	// Organize data
-	var artistInfos []ArtistInfo
-	for _, relation := range relations {
-		artistInfo := ArtistInfo{
-			Artist:   artists[relation.ID],
-			Location: locations[relation.ID].Locations,
-			Dates:    dates[relation.ID].Dates,
-		}
-		artistInfos = append(artistInfos, artistInfo)
-	}
-
-	return artistInfos, nil
-}
